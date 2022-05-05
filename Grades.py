93b544bd237e14bc8263704413abd6ad80f96410
Brandon Hang <brandonhang34@outlook.com>
2022-05-05 15:39:32 -0700

    Structured English commit

---
+++ Grades.py
@@ -0,0 +1,12 @@
+
+
+
+"""
+STRUCTURED ENGLISH
+
+In this program, we will create a program that will show
+the number grades, the average grade, and the percentage of 
+grades that are above the average grade. Using the Final.txt
+file, that will be utilized as a source of inputs in order to 
+perform the following program.
+"""
